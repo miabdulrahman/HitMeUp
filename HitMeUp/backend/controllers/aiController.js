289a29d63bd092a67fef20bf72e00598590d9c1b
prabhath3 <prabhathnishantha130@gmail.com>
2026-09-14 22:31:33 +0530

    ai controller issue solved

--- HitMeUp/backend/controllers/aiController.js
+++ HitMeUp/backend/controllers/aiController.js
@@ -47,6 +47,38 @@ const generateDealController = async (req, res) => {
   }
 };
 
+const recommendDealsController = async (req , res) => {
+    try {
+        const {userPreferences, availableDeals,} = req.body;
+
+        if(!userPreferences || !availableDeals){
+            return res.status(400).json({
+                message: "userPreferences and availableDeals are required",
+            });
+        }
+
+        const recommendations = await recommendDeals({
+            userPreferences,
+            availableDeals,
+        });
+
+        return res.status(200).json({
+            success:true,
+            recommendations,
+        })
+
+    }catch(error){
+        console.log("Reccomment Deal Error: ", error);
+
+        return res.status(500).json({
+            success:false,
+            message:"Failed to generate deal recommendations",
+        })
+
+    }
+}
+
 module.exports = {
   generateDealController,
+  recommendDealsController,
 };

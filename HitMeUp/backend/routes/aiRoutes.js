bc1ff2963889aae5922a88b4e620050786c06149
prabhath3 <prabhathnishantha130@gmail.com>
2026-09-19 09:49:07 +0530

    update ai routes to add detectfraudController

--- HitMeUp/backend/routes/aiRoutes.js
+++ HitMeUp/backend/routes/aiRoutes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
     generateDealController,
     recommendDealsController,
+    detectFraudController,
 } = require("../controllers/aiController");
 
 const router = express.Router();
 
 router.post("/generate-deal", generateDealController);
 router.post("/recommend-deals", recommendDealsController);
+router.post("/detect-fraud", detectFraudController);
 
 module.exports = router;

a0d53a15f10a62c470ecc32c409ec6202d7c5585
prabhath3 <prabhathnishantha130@gmail.com>
2026-09-26 20:46:15 +0530

    add customer dashboard skeleton

--- HitMeUp/frontend/src/App.jsx
+++ HitMeUp/frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import CustomerDashBoard from "./pages/customer/CustomerDashboard";
 
 function App() {
-  return(
-    <LandingPage/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="customer/dashboard" element={<CustomerDashBoard />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
